d4da7291c72587185b371118aa05a61e86941385
Crustbrns <chuenko04@gmail.com>
2023-06-17 12:19:55 +0300

    Added significant classes for logic's hierarchy

--- scripts/checkers.js
+++ scripts/checkers.js
@@ -21,3 +21,42 @@ const TempMove = {
 }
 const Letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const Numbers = ['8', '7', '6', '5', '4', '3', '2', '1'];
+
+class BeatMove {
+    constructor(checker, enemychecker, fielddestination) {
+        this.checker = checker;
+        this.enemychecker = enemychecker;
+        this.fielddestination = fielddestination;
+    }
+}
+
+class Move {
+    constructor(moveNum, color, startPos, finalPos) {
+        this.moveNum = moveNum;
+        this.color = color;
+        this.startPos = startPos;
+        this.finalPos = finalPos;
+    }
+}
+
+function InitGame() {
+    setDragging('black', false);
+    setDragging('white', true);
+}
+
+function ChangePlayer() {
+    Game.CurrentPlayer = Game.CurrentPlayer == Players.One ? Players.Two : Players.One;
+    if (Game.CurrentPlayer == Players.One) {
+        setDragging('black', false);
+        setDragging('white', true);
+        // calcMoves('white');
+    }
+    else {
+        setDragging('black', true);
+        setDragging('white', false);
+        Game.CurrentMove++;
+        // calcMoves('black');
+    }
+}
+
+InitGame();
