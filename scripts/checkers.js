1fa36469f150f6396cea0ee5bae868029d5355d2
Crustbrns <chuenko04@gmail.com>
2023-06-18 09:34:03 +0300

    Added log into the game, changed play logic
    All the moves are tracking via log now, holding the player's colour and two destinations, added beat logic and final checks

--- scripts/checkers.js
+++ scripts/checkers.js
@@ -250,4 +250,340 @@ function tryAddMove(dragevent) {
     }
 }
 
+function drag(dragevent) {
+    MouseMove(true);
+    removeGraphMoves();
+
+    if (Game.CurrentPlayer == Players.One && dragevent.target.classList.contains('white')) {
+        tryAddMove(dragevent);
+        if (Game.RequiredToBeat) {
+            displayBeatMoves(dragevent.target);
+            dragevent.dataTransfer.setData('div', dragevent.target.id);
+            dragevent.dataTransfer.dropEffect = "copy";
+            setDragCursor(false);
+            TempMove.startPos = dragevent.target.parentElement.id;
+        }
+        else {
+            calcDistinctMoves(dragevent.target.parentElement.id, 'white');
+            dragevent.dataTransfer.setData('div', dragevent.target.id);
+            dragevent.dataTransfer.dropEffect = "copy";
+            setDragCursor(false);
+            TempMove.startPos = dragevent.target.parentElement.id;
+        }
+    }
+    else if (Game.CurrentPlayer == Players.Two && dragevent.target.classList.contains('black')) {
+        tryAddMove(dragevent);
+        if (Game.RequiredToBeat) {
+            displayBeatMoves(dragevent.target);
+            dragevent.dataTransfer.setData('div', dragevent.target.id);
+            dragevent.dataTransfer.dropEffect = "copy";
+            setDragCursor(false);
+            TempMove.startPos = dragevent.target.parentElement.id;
+        }
+        else {
+            calcDistinctMoves(dragevent.target.parentElement.id, 'black');
+            dragevent.dataTransfer.setData('div', dragevent.target.id);
+            setDragCursor(false);
+            TempMove.startPos = dragevent.target.parentElement.id;
+        }
+    }
+}
+function dragend(dragevent) {
+    removeGraphMoves();
+    removeActive();
+}
+function setDragging(color, truth) {
+    let elements = Array.from(document.getElementsByClassName(color));
+    for (const item of elements) {
+        item.draggable = truth;
+        if (truth == false) {
+            item.removeEventListener('dragstart', function () { drag(event), false });
+            item.removeEventListener('dragend', function () { dragend(event), false });
+            item.classList.remove('isdraggable');
+        } else {
+            item.addEventListener('dragstart', function () { drag(event), false });
+            item.addEventListener('dragend', function () { dragend(event), false });
+            item.classList.add('isdraggable');
+        }
+    }
+}
+
+function checkPos(checker) {
+    if (!checker.classList.contains('crown')
+        && ((Game.CurrentPlayer == Players.One && checker.parentElement.id >= 56) ||
+            (Game.CurrentPlayer == Players.Two && checker.parentElement.id <= 7))) {
+        checker.classList.add('crown');
+    }
+}
+
+function setPartialDragging(elements, truth) {
+    for (const item of elements) {
+        item.checker.draggable = truth;
+        if (truth == false) {
+            item.checker.removeEventListener('dragstart', function () { drag(event), false });
+            item.checker.removeEventListener('dragend', function () { dragend(event), false });
+            item.checker.classList.remove('isdraggable');
+        } else {
+            item.checker.addEventListener('dragstart', function () { drag(event), false });
+            item.checker.addEventListener('dragend', function () { dragend(event), false });
+            item.checker.classList.add('isdraggable');
+        }
+    }
+}
+
+function ClearTempMove() {
+    TempMove.startPos = null;
+    TempMove.finalPos = null;
+}
+function addInHistory() {
+    let color = Game.CurrentPlayer == Players.One ? 'black' : 'white';
+    Game.HistoryMoves.push(new Move(Game.CurrentMove, color, TempMove.startPos, TempMove.finalPos));
+    ClearTempMove();
+    displayHistory();
+}
+function displayHistory() {
+    let move = Game.HistoryMoves.slice(-1);
+    let log = document.getElementById('log-container');
+
+    let container = document.createElement('div');
+    container.className = 'move-container';
+
+    let color = document.createElement('div');
+    color.className = 'color';
+    color.textContent = move[0].color;
+
+    let index = document.createElement('index');
+    index.textContent = move[0].moveNum;
+
+    let tempmove = document.createElement('div');
+    tempmove.className = 'move';
+    tempmove.textContent = `${convertToNum(move[0].startPos)} > ${convertToNum(move[0].finalPos)}`;
+
+    container.appendChild(index);
+    container.appendChild(color);
+    container.appendChild(tempmove);
+
+    log.appendChild(container);
+
+    log.lastChild.scrollIntoView({ behavior: 'smooth' });
+}
+function convertToNum(index) {
+    let num = Numbers[Math.floor(index / 8)];
+    let lit = Letters[Math.floor(index % 8)];
+    return `${lit}${num}`
+}
+function updateTitle() {
+    let title = document.getElementById('title');
+    title.textContent = `Ход: ${Game.CurrentPlayer == Players.One ? 'Белые' : 'Черные'}`;
+}
+function drop(dropevent) {
+    MouseMove(false);
+    dropevent.preventDefault();
+    removeActive();
+
+    let field = null;
+    if (Game.AvailableMoves.includes(dropevent.target)) field = dropevent.target;
+    else if (Game.AvailableMoves.includes(dropevent.target.parentElement)) field = dropevent.target.parentElement;
+
+    removeGraphMoves();
+    if ((field != null && !Game.RequiredToBeat) || (field != null && Game.RequiredToBeat
+        && Game.AvailableCheckers.find(x => x.fielddestination == field).checker.parentElement.id == TempMove.startPos)) {
+
+        if (!field.classList.contains('checker') &&
+            field.children.length == 0) {
+
+            tryKillChecker(field);
+
+            var data = dropevent.dataTransfer.getData('div');
+            field.appendChild(document.getElementById(data));
+
+            if (Game.RequiredToBeat) {
+                checkPartialBeatMoves(field.children[0]);
+            }
+            checkPos(document.getElementById(data));
+            if (Game.RequiredToBeat) {
+                updateTitle();
+                TempMove.finalPos = field.id;
+                addInHistory();
+            }
+            else {
+                ChangePlayer();
+                updateTitle();
+                TempMove.finalPos = field.id;
+                addInHistory();
+                checkBeatMoves();
+            }
+            checkForWin();
+            play();
+        }
+    }
+    setDragCursor(false);
+}
+
+function play() {
+    let audio = new Audio('./scripts/move-self.mp3');
+    audio.play();
+}
+
+function checkForWin() {
+    let whitecheckers = document.getElementsByClassName('white');
+    let blackcheckers = document.getElementsByClassName('black');
+
+    if (whitecheckers.length == 0 || blackcheckers.length == 0) {
+        let log = document.getElementById('log');
+        let resultTitle = document.createElement('div');
+        resultTitle.textContent = 'Game Over';
+        resultTitle.className = 'result-title';
+        log.appendChild(resultTitle);
+
+        let title = document.getElementById('title');
+        title.textContent = `${whitecheckers.length == 0 ? 'Black' : 'White'} is Winner`;
+    }
+}
+
+function checkBeatMoves() {
+    let color = Game.CurrentPlayer == Players.One ? 'white' : 'black'
+    let checkers = Array.from(document.getElementsByClassName(color));
+    let enemies = Array.from(document.getElementsByClassName(color == 'white' ? 'black' : 'white'));
+    let fields = Array.from(document.getElementsByClassName('chess-field'));
+    let availablecheckers = [];
+    let beatMoves = [];
+    Game.RequiredToBeat = false;
+
+    for (const item of checkers) {
+        let upperleft = enemies.find(x => x.parentElement.id == parseInt(item.parentElement.id) - 9);
+        if (upperleft != undefined) {
+            if (upperleft.classList.contains('checker') && !upperleft.classList.contains(color)) {
+                let field = fields.find(x => x.id == parseInt(upperleft.parentElement.id) - 9);
+                if (field != undefined && field.children.length == 0 && field.style.backgroundColor != 'var(--field-background-white-old)') {
+                    beatMoves.push(field);
+                    availablecheckers.push(new BeatMove(item, upperleft, field));
+                    console.log(availablecheckers);
+                }
+            }
+        }
+
+        let upperright = enemies.find(x => x.parentElement.id == parseInt(item.parentElement.id) - 7);
+        if (upperright != undefined) {
+            if (upperright.classList.contains('checker') && !upperright.classList.contains(color)) {
+                let field = fields.find(x => x.id == parseInt(upperright.parentElement.id) - 7);
+                if (field != undefined && field.children.length == 0 && field.style.backgroundColor != 'var(--field-background-white-old)') {
+                    beatMoves.push(field);
+                    availablecheckers.push(new BeatMove(item, upperright, field));
+                    console.log(availablecheckers);
+                }
+            }
+        }
+
+        let bottomleft = enemies.find(x => x.parentElement.id == parseInt(item.parentElement.id) + 7);
+        if (bottomleft != undefined) {
+            if (bottomleft.classList.contains('checker') && !bottomleft.classList.contains(color)) {
+                let field = fields.find(x => x.id == parseInt(bottomleft.parentElement.id) + 7);
+                if (field != undefined && field.children.length == 0 && field.style.backgroundColor != 'var(--field-background-white-old)') {
+                    beatMoves.push(field);
+                    availablecheckers.push(new BeatMove(item, bottomleft, field));
+                    console.log(availablecheckers);
+                }
+            }
+        }
+
+        let bottomright = enemies.find(x => x.parentElement.id == parseInt(item.parentElement.id) + 9);
+        if (bottomright != undefined) {
+            if (bottomright.classList.contains('checker') && !bottomright.classList.contains(color)) {
+                let field = fields.find(x => x.id == parseInt(bottomright.parentElement.id) + 9);
+                if (field != undefined && field.children.length == 0 && field.style.backgroundColor != 'var(--field-background-white-old)') {
+                    beatMoves.push(field);
+                    availablecheckers.push(new BeatMove(item, bottomright, field));
+                    console.log(availablecheckers);
+                }
+            }
+        }
+    }
+
+    if (beatMoves.length > 0) {
+        Game.RequiredToBeat = true;
+        Game.AvailableMoves = beatMoves;
+        Game.AvailableCheckers = availablecheckers;
+        setDragging('white', false);
+        setDragging('black', false);
+        setPartialDragging(availablecheckers, true);
+    }
+}
+
+function checkPartialBeatMoves(checker) {
+    let color = Game.CurrentPlayer == Players.One ? 'white' : 'black'
+    let enemies = Array.from(document.getElementsByClassName(color == 'white' ? 'black' : 'white'));
+    let fields = Array.from(document.getElementsByClassName('chess-field'));
+    let availablecheckers = [];
+    let beatMoves = [];
+    Game.RequiredToBeat = false;
+
+    let upperleft = enemies.find(x => x.parentElement.id == parseInt(checker.parentElement.id) - 9);
+    if (upperleft != undefined) {
+        if (upperleft.classList.contains('checker') && !upperleft.classList.contains(color)) {
+            let field = fields.find(x => x.id == parseInt(upperleft.parentElement.id) - 9);
+            if (field != undefined && field.children.length == 0 && field.style.backgroundColor != 'var(--field-background-white-old)') {
+                beatMoves.push(field);
+                availablecheckers.push(new BeatMove(checker, upperleft, field));
+                console.log(availablecheckers);
+            }
+        }
+    }
+
+    let upperright = enemies.find(x => x.parentElement.id == parseInt(checker.parentElement.id) - 7);
+    if (upperright != undefined) {
+        if (upperright.classList.contains('checker') && !upperright.classList.contains(color)) {
+            let field = fields.find(x => x.id == parseInt(upperright.parentElement.id) - 7);
+            if (field != undefined && field.children.length == 0 && field.style.backgroundColor != 'var(--field-background-white-old)') {
+                beatMoves.push(field);
+                availablecheckers.push(new BeatMove(checker, upperright, field));
+                console.log(availablecheckers);
+            }
+        }
+    }
+
+    let bottomleft = enemies.find(x => x.parentElement.id == parseInt(checker.parentElement.id) + 7);
+    if (bottomleft != undefined) {
+        if (bottomleft.classList.contains('checker') && !bottomleft.classList.contains(color)) {
+            let field = fields.find(x => x.id == parseInt(bottomleft.parentElement.id) + 7);
+            if (field != undefined && field.children.length == 0 && field.style.backgroundColor != 'var(--field-background-white-old)') {
+                beatMoves.push(field);
+                availablecheckers.push(new BeatMove(checker, bottomleft, field));
+                console.log(availablecheckers);
+            }
+        }
+    }
+
+    let bottomright = enemies.find(x => x.parentElement.id == parseInt(checker.parentElement.id) + 9);
+    if (bottomright != undefined) {
+        if (bottomright.classList.contains('checker') && !bottomright.classList.contains(color)) {
+            let field = fields.find(x => x.id == parseInt(bottomright.parentElement.id) + 9);
+            if (field != undefined && field.children.length == 0 && field.style.backgroundColor != 'var(--field-background-white-old)') {
+                beatMoves.push(field);
+                availablecheckers.push(new BeatMove(checker, bottomright, field));
+                console.log(availablecheckers);
+            }
+        }
+    }
+
+    if (beatMoves.length > 0) {
+        Game.RequiredToBeat = true;
+        Game.AvailableMoves = beatMoves;
+        Game.AvailableCheckers = availablecheckers;
+        setDragging('white', false);
+        setDragging('black', false);
+        setPartialDragging(availablecheckers, true);
+    }
+}
+
+function tryKillChecker(field) {
+    if (Game.RequiredToBeat) {
+        let enemyfield = document.getElementById(Game.AvailableCheckers.find(x => x.fielddestination == field).enemychecker.id);
+        enemyfield.remove();
+    }
+}
+
+setDraggable();
+fillField();
 InitGame();
+updateTitle();
