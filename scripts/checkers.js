35eafffd43e416699418e73c109fb803bab7e100
Crustbrns <chuenko04@gmail.com>
2023-06-18 09:30:50 +0300

    Added logic to handle inactive checkers
    Some functions to restrict moving inactive checkers

--- scripts/checkers.js
+++ scripts/checkers.js
@@ -130,4 +130,124 @@ function displayBeatMoves(target) {
     console.log(Game.AvailableMoves);
 }
 
+
+function removeActive() {
+    let res = Array.from(document.getElementsByClassName('active'));
+    for (const item of res) {
+        item.classList.remove('active');
+    }
+}
+
+function removeGraphMoves() {
+    let res = document.getElementsByClassName('movement');
+    while (res.length > 0) {
+        res[0].parentNode.removeChild(res[0]);
+    }
+}
+function setDraggable() {
+    let res = document.getElementsByClassName('chess-field');
+
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (i % 2) {
+                if (j % 2 == false) {
+                    res[i * 8 + j].addEventListener('drop', function () { drop(event) });
+                    res[i * 8 + j].addEventListener('dragover', function () { allowDrop(event) });
+                }
+            }
+            else {
+                if (j % 2) {
+                    res[i * 8 + j].addEventListener('drop', function () { drop(event) });
+                    res[i * 8 + j].addEventListener('dragover', function () { allowDrop(event) });
+                }
+            }
+        }
+    }
+}
+function fillField() {
+    let fields = document.getElementsByClassName('chess-field');
+
+    let index = 0;
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if ((i * 8 + j < 24) || (i * 8 + j > 39)) {
+                if (i % 2) {
+                    if (j % 2 == false) {
+                        fields[i * 8 + j].appendChild(setChecker(i, index));
+                        index++;
+                    }
+                }
+                else {
+                    if (j % 2) {
+                        fields[i * 8 + j].appendChild(setChecker(i, index));
+                        index++;
+                    }
+                }
+            }
+        }
+    }
+}
+function setChecker(index, checkerindex) {
+    let checker = document.createElement("div");
+    checker.className = 'checker';
+    checker.draggable = true;
+    checker.id = `ch${checkerindex}`;
+
+    checker.addEventListener('dragstart', function () { drag(event), false });;
+    checker.addEventListener('mousedown', function () { MouseMove(true) });
+
+    if (index > 2) {
+        checker.classList.add('black');
+    }
+    else {
+        checker.classList.add('white');
+    }
+    return checker;
+}
+
+const setDragCursor = value => {
+    const body = document.getElementsByTagName('body').item(0);
+    body.classList.toggle('grabbing', value);
+}
+function allowDrop(dragevent) {
+    dragevent.preventDefault();
+}
+
+function getElements(ev) {
+    let elements = document.getElementsByClassName('field-active');
+    for (let item of elements) {
+        item.classList.remove('field-active');
+    }
+    let elemBelow = document.elementsFromPoint(ev.clientX, ev.clientY);
+    let elemNeeded = elemBelow.find(x => x.className == 'chess-field' && x.children[0] != null && x.children[0].className == 'movement');
+    if (elemNeeded != null && !elemNeeded.classList.contains('field-active')) elemNeeded.classList.add('field-active');
+    // console.log();
+}
+function MouseMove(toggle) {
+    if (toggle) {
+        document.addEventListener('mouseover', function (event) {
+            getElements(event);
+        })
+    }
+    else {
+        document.removeEventListener('mouseover', function (event) {
+            getElements(event);
+        })
+    }
+
+}
+
+function tryAddMove(dragevent) {
+    if (Game.RequiredToBeat) {
+        console.log(Game.AvailableCheckers);
+        console.log(dragevent.target);
+        if (Game.AvailableCheckers.find(x => x.checker == dragevent.target) != undefined) {
+            dragevent.target.parentElement.classList.add('active');
+        }
+    }
+    else {
+        dragevent.target.parentElement.classList.add('active');
+    }
+}
+
 InitGame();
